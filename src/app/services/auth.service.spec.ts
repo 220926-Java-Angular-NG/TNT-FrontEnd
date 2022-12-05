38f19f88391071e176ad9bf81d2214d0792fd626
Jonathan Bedoy <jonathanbedoy1@gmail.com>
2022-12-05 11:58:27 -0800

    updated logos

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClient } from '@angular/common/http';
 
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    httpClientSpy = jasmine.createSpyObj<HttpClient>('HttpClient',['get','post','delete', 'put']);
+    TestBed.configureTestingModule({
+      providers:[{provide:HttpClient, useValue: httpClientSpy}]
+    });
     service = TestBed.inject(AuthService);
   });
 
