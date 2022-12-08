6d1d28a99f72958fe793d4c6b12f5e96a8bae75e
Jonathan Bedoy <jonathanbedoy1@gmail.com>
2022-12-08 07:38:14 -0800

    updated aws url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   withCredentials: true,
   // baseUrl: "http://localhost:8080",
-  baseUrl: "http://ec2-54-205-23-209.compute-1.amazonaws.com",
+  baseUrl: "http://ec2-54-146-255-45.compute-1.amazonaws.com",
   headers: {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': 'http://tntfrontendrev.s3-website-us-east-1.amazonaws.com',
